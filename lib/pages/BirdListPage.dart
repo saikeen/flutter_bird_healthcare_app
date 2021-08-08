70172e6304340ecff7c7586c7a88bb16e9e9182f
kenji saito <32677139+saikeen@users.noreply.github.com>
2021-08-09 02:14:34 +0900

    Supports integration with Firestore in the list screen.

--- lib/pages/BirdListPage.dart
+++ lib/pages/BirdListPage.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:BirdHealthcare/services/NavigationService.dart';
 import 'package:BirdHealthcare/settings/ScreenArguments.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BirdListPage extends StatefulWidget {
@@ -21,24 +22,10 @@ class BirdListPage extends StatefulWidget {
 class _BirdListPageState extends State<BirdListPage> {
   @override
   Widget build(BuildContext context) {
-    final birds = [
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-    ];
+    final Stream<QuerySnapshot> _birdsStream = FirebaseFirestore.instance
+                                                                .collection('birds')
+                                                                .orderBy('createdAt', descending: false)
+                                                                .snapshots();
 
     return Scaffold(
       appBar: AppBar(
@@ -47,50 +34,64 @@ class _BirdListPageState extends State<BirdListPage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: birds.map((bird) =>
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ListTile(
-                    title: Text(bird.name),
-                    subtitle: Text("生年月日: 20xx/xx/xx"),
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.grey.shade200,
-                      child: ClipOval(
-                        child: Image.network(
-                          bird.imgUrl,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _birdsStream,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+
+            return ListView(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                return Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(data['name']),
+                        subtitle: Text("生年月日: 20xx/xx/xx"),
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.grey.shade200,
+                          child: ClipOval(
+                            child: Image.network(
+                              data['imageUrl'],
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    trailing: Wrap(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () => {
-                            //タブ内に遷移
-                            NavigationService.pushInTab(
-                              "/bird_edit",
-                              arguments: ScreenArguments(
-                                DateTime.now().toIso8601String(),
-                              ),
-                            )
-                          },
+                        trailing: Wrap(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () => {
+                                //タブ内に遷移
+                                NavigationService.pushInTab(
+                                  "/bird_edit",
+                                  arguments: ScreenArguments(
+                                    DateTime.now().toIso8601String(),
+                                  ),
+                                )
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                // タップ時の処理
+                              },
+                            ),
+                          ],
                         ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {
-                            // タップ時の処理
-                          },
-                        ),
-                      ],
-                    ),
+                      )
+                    ],
                   )
-                ],
-              )
-            )
-          ).toList(),
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -109,13 +110,4 @@ class _BirdListPageState extends State<BirdListPage> {
       ),
     );
   }
-}
-
-class Bird {
- final String name;
- final String imgUrl;
- const Bird({
-   @required this.name,
-   @required this.imgUrl,
- });
 }
--- lib/pages/RecordListPage.dart
+++ lib/pages/RecordListPage.dart
@@ -22,89 +22,6 @@ class RecordListPage extends StatefulWidget {
 class _RecordListPageState extends State<RecordListPage> {
   @override
   Widget build(BuildContext context) {
-    final birds = [
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-      Bird(
-        name: "BirdName01",
-        imgUrl: 'https://3.bp.blogspot.com/-Ncn2Gj8Aq9k/WwJZjGt-FBI/AAAAAAABMF0/5Uco6MFSragyb_xcDgrfuUFZMfx9diW2gCLcBGAs/s400/bird_okameinkogray.png',
-      ),
-      Bird(
-        name: 'BirdName02',
-        imgUrl: 'https://1.bp.blogspot.com/-dkBk4bYQrTk/XVKfloSYxiI/AAAAAAABUC8/j6K3SGQG0WMxKFn71LzznPz0SPgI5ufGQCLcBGAs/s400/bird_sekisei_inko_blue.png',
-      ),
-      Bird(
-        name: 'BirdName03',
-        imgUrl: 'https://1.bp.blogspot.com/-Nqqq-_PR3oc/Xb-Z7SW7EhI/AAAAAAABV-c/Y_sr_rTdjBAYdWhInk3wS7U_4z2tTMKaQCNcBGAsYHQ/s400/bird_inko_sakura.png',
-      ),
-      Bird(
-        name: 'BirdName04',
-        imgUrl: 'https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s400/bird_mameruriha_inko_blue.png',
-      ),
-    ];
-
     final bodyWeightData = [
       new BodyWeightData(new DateTime(2021, 8, 8), 35.0),
       new BodyWeightData(new DateTime(2021, 8, 9), 35.3),
@@ -151,7 +68,10 @@ class _RecordListPageState extends State<RecordListPage> {
       return series;
     }
 
-    final Stream<QuerySnapshot> _birdsStream = FirebaseFirestore.instance.collection('birds').snapshots();
+    final Stream<QuerySnapshot> _birdsStream = FirebaseFirestore.instance
+                                                                .collection('birds')
+                                                                .orderBy('createdAt', descending: false)
+                                                                .snapshots();
 
     return Scaffold(
       appBar: AppBar(
@@ -289,15 +209,6 @@ class _RecordListPageState extends State<RecordListPage> {
       ),
     );
   }
-}
-
-class Bird {
- final String name;
- final String imgUrl;
- const Bird({
-   @required this.name,
-   @required this.imgUrl,
- });
 }
 
 class BodyWeightData {
