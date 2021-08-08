04a805777e6ce4b855c6718e8670aa8a177e499e
kenji saito <32677139+saikeen@users.noreply.github.com>
2021-08-08 20:57:44 +0900

    Fixed the display of graphs on the home screen

--- lib/pages/RecordListPage.dart
+++ lib/pages/RecordListPage.dart
@@ -1,6 +1,7 @@
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'package:BirdHealthcare/services/NavigationService.dart';
 import 'package:BirdHealthcare/settings/ScreenArguments.dart';
 
 class RecordListPage extends StatefulWidget {
@@ -104,13 +105,13 @@ class _RecordListPageState extends State<RecordListPage> {
     ];
 
     final bodyWeightData = [
-      new BodyWeightData(new DateTime(2021, 8, 8), 3),
-      new BodyWeightData(new DateTime(2021, 8, 9), 3),
-      new BodyWeightData(new DateTime(2021, 8, 10), 3),
-      new BodyWeightData(new DateTime(2021, 8, 11), 3),
-      new BodyWeightData(new DateTime(2021, 8, 12), 3),
-      new BodyWeightData(new DateTime(2021, 8, 13), 3),
-      new BodyWeightData(new DateTime(2021, 8, 14), 3),
+      new BodyWeightData(new DateTime(2021, 8, 8), 35.0),
+      new BodyWeightData(new DateTime(2021, 8, 9), 35.3),
+      new BodyWeightData(new DateTime(2021, 8, 10), 35.3),
+      new BodyWeightData(new DateTime(2021, 8, 11), 35.0),
+      new BodyWeightData(new DateTime(2021, 8, 12), 34.5),
+      new BodyWeightData(new DateTime(2021, 8, 13), 34.5),
+      new BodyWeightData(new DateTime(2021, 8, 14), 35.0),
     ];
 
     final foodWeightData = [
@@ -155,24 +156,34 @@ class _RecordListPageState extends State<RecordListPage> {
         elevation: 0,
       ),
       body: Container(
-        decoration: BoxDecoration(border: Border.all(
-            color: Colors.red,
-            width: 8.0,
-          ),
-        ),
+        // decoration: BoxDecoration(border: Border.all(
+        //     color: Colors.red,
+        //     width: 8.0,
+        //   ),
+        // ),
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Container(
-              height: 80.0,
-              decoration: BoxDecoration(border: Border.all(
-                  color: Colors.blue,
-                  width: 8.0,
-                ),
-              ),
+              height: 60.0,
+              // decoration: BoxDecoration(border: Border.all(
+              //     color: Colors.blue,
+              //     width: 8.0,
+              //   ),
+              // ),
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: birds.map((bird) =>
+                  // ElevatedButton(
+                  //     onPressed: () {},
+                  //     child: Icon(Icons.add, color: Colors.white),
+                  //     style: ElevatedButton.styleFrom(
+                  //       shape: CircleBorder(),
+                  //       padding: EdgeInsets.all(20),
+                  //       primary: Colors.blue,
+                  //       onPrimary: Colors.white,
+                  //     ),
+                  // ),
                   Stack(
                     children: <Widget>[
                       CircleAvatar(
@@ -199,12 +210,12 @@ class _RecordListPageState extends State<RecordListPage> {
               ),
             ),
             Container(
-              height: 300.0,
-              decoration: BoxDecoration(border: Border.all(
-                  color: Colors.blue,
-                  width: 8.0,
-                ),
-              ),
+              height: 250.0,
+              // decoration: BoxDecoration(border: Border.all(
+              //     color: Colors.blue,
+              //     width: 8.0,
+              //   ),
+              // ),
               child: Card(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -226,12 +237,12 @@ class _RecordListPageState extends State<RecordListPage> {
               )
             ),
             Container(
-              height: 300.0,
-              decoration: BoxDecoration(border: Border.all(
-                  color: Colors.blue,
-                  width: 8.0,
-                ),
-              ),
+              height: 250.0,
+              // decoration: BoxDecoration(border: Border.all(
+              //     color: Colors.blue,
+              //     width: 8.0,
+              //   ),
+              // ),
               child: Card(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -255,6 +266,20 @@ class _RecordListPageState extends State<RecordListPage> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => {
+          //タブ内に遷移
+          NavigationService.pushInTab(
+            "/record_registration",
+            arguments: ScreenArguments(
+              DateTime.now().toIso8601String(),
+            ),
+          )
+        },
+        label: const Text('追加'),
+        icon: const Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
     );
   }
 }
@@ -270,7 +295,7 @@ class Bird {
 
 class BodyWeightData {
   final DateTime time;
-  final int sales;
+  final double sales;
 
   BodyWeightData(this.time, this.sales);
 }
