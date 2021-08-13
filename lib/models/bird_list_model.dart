7cc0c56e187c7f0c78935f104b18261a53f961f8
kenji saito <32677139+saikeen@users.noreply.github.com>
2021-08-13 09:23:47 +0900

    Get data only once from Firestore with the get method

--- lib/models/bird_list_model.dart
+++ lib/models/bird_list_model.dart
@@ -3,23 +3,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BirdListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
-                                                                .collection('birds')
-                                                                .orderBy('createdAt', descending: false)
-                                                                .snapshots();
-
   List<Bird> birds;
 
-  void fetchBirdList(){
-    _usersStream.listen((QuerySnapshot snapshot) {
-      final List<Bird> birds = snapshot.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-        final String name = data['name'];
-        final String imageUrl = data['imageUrl'];
-        return Bird(name, imageUrl);
-      }).toList();
-      this.birds = birds;
-      notifyListeners();
-    });
+  void fetchBirdList() async {
+
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('birds').orderBy('createdAt', descending: false).get();
+
+    final List<Bird> birds = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String name = data['name'];
+      final String imageUrl = data['imageUrl'];
+      return Bird(name, imageUrl);
+    }).toList();
+
+    this.birds = birds;
+    notifyListeners();
   }
 }
